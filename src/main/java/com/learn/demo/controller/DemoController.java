50580576077f655b09d77c782a5d548b7fc99e21
zengqingquan <zengqingquan018@163.com>
2019-11-07 10:24:08 +0800

    first

---
+++ src/main/java/com/learn/demo/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.learn.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World";
+    }
+}
