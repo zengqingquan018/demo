a7bf5adc781059a23d68f874a5ae6b340ae8258f
zengqingquan018@163.com <zengqingquan018@163.com>
2020-05-28 14:18:23 +0800

    工具类

---
+++ demoCode/src/main/java/com/example/demo/common/utils/DateUtils.java
@@ -0,0 +1,288 @@
+package com.example.demo.common.utils;
+
+
+import io.micrometer.core.instrument.util.StringUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * @Author ZQQ
+ * @Date 2020/5/14 19:55
+ */
+public class DateUtils {
+
+    public static final String DATE_TIME = "yyyy-MM-dd HH:mm:ss";
+
+    public static final String DATE = "yyyy-MM-dd";
+
+    public static final String DATE_TIME_CH = "yyyy年MM月dd日";
+
+    public static final String DATE_TIME_FORMAT_yyyyMMddHHmmss = "yyyyMMddHHmmss";
+
+    public static final String DATE_yyyyMMdd = "yyyyMMdd";
+
+    /**
+     * 格式化日期字符串
+     *
+     * @param date    日期对应的字符串 格式为yyyy-MM-dd HH:mm:ss
+     * @param format 格式
+     * @return 若传入的格式正确，返回Date
+     */
+    public static String formatDataString(String date, String format) {
+        return format(parseDateTime(date), format);
+    }
+
+    /**
+     * 根据Date类型日期获取String类型日期
+     *
+     * @param date    Date类型日期
+     * @param format 格式
+     * @return 若传入的格式正确，返回String类型的日期
+     */
+    public static String format(Date date, String format) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat(format);
+        return dateFormat.format(date);
+    }
+
+    /**
+     * 根据Date类型日期获取String类型日期 格式为yyyy-MM-dd HH:mm:ss
+     *
+     * @param date    Date类型日期
+     * @return 若传入的格式正确，返回String类型的日期
+     */
+    public static String formatDateTime(Date date) {
+        return null == date ? null : format(date, DATE_TIME);
+    }
+
+    /**
+     * 根据Date类型日期获取String类型日期 格式为yyyy-MM-dd
+     *
+     * @param date    Date类型日期
+     * @return 若传入的格式正确，返回String类型的日期
+     */
+    public static String formatDate(Date date) {
+        return null == date ? null : format(date, DATE);
+    }
+
+    /**
+     * 时间字符串格式化时间
+     * @param dateStr   时间字符串
+     * @param format   时间格式
+     * @return Date
+     */
+    public static Date parse(String dateStr, String format) {
+        try {
+            dateStr = normalize(dateStr);
+            return new SimpleDateFormat(format).parse(dateStr);
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    /**
+     * 时间字符串转date 格式为yyyy-MM-dd HH:mm:ss
+     * @param dateString  时间字符串
+     * @return Date
+     */
+    public static Date parseDateTime(String dateString) {
+        dateString = normalize(dateString);
+        return parse(dateString, DATE_TIME);
+    }
+
+    /**
+     * 时间字符串转date 格式为yyyy-MM-dd
+     * @param dateString  时间字符串
+     * @return Date
+     */
+    public static Date parseDate(String dateString) {
+        dateString = normalize(dateString);
+        return parse(dateString, DATE);
+    }
+
+    private static String normalize(String dateStr) {
+        if (StringUtils.isBlank(dateStr)) {
+            return dateStr;
+        } else {
+            List<String> dateAndTime = new ArrayList<>(Arrays.asList(dateStr.split(" ")));
+            int size = dateAndTime.size();
+            if (size >= 1 && size <= 2) {
+                StringBuilder builder = new StringBuilder();
+                String datePart = dateAndTime.get(0).replaceAll("[\\/.年月]", "-");
+                datePart = datePart.replaceAll("日", "");
+                builder.append(datePart);
+                if (size == 2) {
+                    builder.append(" ");
+                    String timePart = dateAndTime.get(1).replaceAll("[时分秒]", ":");
+                    if(timePart.endsWith(":")){
+                        timePart = timePart.substring(0, timePart.length() - 1 );
+                    }
+                    builder.append(timePart);
+                }
+
+                return builder.toString();
+            } else {
+                return dateStr;
+            }
+        }
+    }
+
+    /**
+     * 获取当前时间字符串 格式yyyy-MM-dd HH:mm:ss
+     */
+    public static String now(){
+        return format(new Date(), DATE_TIME);
+    }
+
+    /**
+     * date 转calender
+     */
+    public static Calendar toCalendar(Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        return cal;
+    }
+
+    /**
+     * 获取时间比较结果
+     * @param date          时间
+     * @param anotherDate   时间
+     * @return Date
+     */
+    public static boolean isBeforeOrEquals(Date date, Date anotherDate) {
+        if (null == date || null == anotherDate) {
+            return false;
+        } else {
+            return date.compareTo(anotherDate) <= 0;
+        }
+    }
+
+    /**
+     * 获取时间比较结果
+     * @param date          时间
+     * @param anotherDate   时间
+     * @return Date
+     */
+    public static boolean isAfterOrEquals(Date date, Date anotherDate) {
+        if (null == date || null == anotherDate) {
+            return false;
+        } else {
+            return date.compareTo(anotherDate) >= 0;
+        }
+    }
+
+    /**
+     * 获取根据偏移单位偏移后的时间
+     * @param date        时间
+     * @param dateFiled   偏移单位
+     * @param offset      偏移量
+     * @return Date
+     */
+    public static Date offset(Date date, int dateFiled, int offset) {
+        Calendar calendar = toCalendar(date);
+        calendar.add(dateFiled, offset);
+        return calendar.getTime();
+    }
+
+    public static Date offsetSecond(Date date, int offset) {
+        return offset(date, Calendar.SECOND, offset);
+    }
+
+    public static Date offsetMinute(Date date, int offset) {
+        return offset(date, Calendar.MINUTE, offset);
+    }
+
+    public static Date offsetHour(Date date, int offset) {
+        return offset(date, Calendar.HOUR_OF_DAY, offset);
+    }
+
+    public static Date offsetDay(Date date, int offset) {
+        return offset(date, Calendar.DAY_OF_YEAR, offset);
+    }
+
+    public static Date offsetMonth(Date date, int offset) {
+        return offset(date, Calendar.MONTH, offset);
+    }
+
+    /**
+     * 获取一天的开始时间
+     * @param date   时间
+     * @return Date
+     */
+    public static Date beginOfDay(Date date) {
+        return beginOfDay(toCalendar(date)).getTime();
+    }
+
+    /**
+     * 获取一天的结束时间
+     * @param date   时间
+     * @return Date
+     */
+    public static Date endOfDay(Date date) {
+        return endOfDay(toCalendar(date)).getTime();
+    }
+
+    public static Calendar beginOfDay(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar;
+    }
+
+    public static Calendar endOfDay(Calendar calendar) {
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+        return calendar;
+    }
+
+    /**
+     * 获取一个月的开始时间
+     * @param date   时间
+     * @return Date
+     */
+    public static Date beginOfMonth(Date date) {
+        return beginOfMonth(toCalendar(date)).getTime();
+    }
+
+    /**
+     * 获取一个月的结束时间
+     * @param date   时间
+     * @return Date
+     */
+    public static Date endOfMonth(Date date) {
+        return endOfMonth(toCalendar(date)).getTime();
+    }
+
+    public static Calendar beginOfMonth(Calendar calendar) {
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        return beginOfDay(calendar);
+    }
+
+    public static Calendar endOfMonth(Calendar calendar) {
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+        return endOfDay(calendar);
+    }
+
+    /**
+     * 时间戳转时间类型
+     * @param time  时间戳字符串
+     * @return  时间格式字符串 yyyy-MM-dd HH:mm:ss
+     */
+    public static Date timeStamp2Date(String time) {
+        Long timeLong = Long.parseLong(time);
+        //要转换的时间格式
+        SimpleDateFormat sdf = new SimpleDateFormat(DATE_TIME);
+        Date date;
+        try {
+            date = sdf.parse(sdf.format(timeLong));
+            return date;
+        } catch (Exception  e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
--- demoCode/src/main/java/com/example/demo/common/utils/ExcelUtils.java
+++ demoCode/src/main/java/com/example/demo/common/utils/ExcelUtils.java
@@ -1,26 +1,31 @@
 package com.example.demo.common.utils;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
 import org.apache.poi.ss.usermodel.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+/**
+ * @Author ZQQ
+ * @Date 2020/2/14 15:55
+ */
 public class ExcelUtils {
-
     /**
      * 描述:获取EXCEL的数据，入参为MultipartFile，出参为List<String []>，String[]为每一行的数据，空行跳过
      *
      * @param file
      * @return {@link List< String[]>}
      * @throws
-     * @author
-     * @date 2020/4/16 10:48
+     * @author ZQQ
+     *
      */
-    public static List<String[]> getExcelData(MultipartFile file) throws IOException, InvalidFormatException {
+    public static List<String[]> getExcelData(MultipartFile file) throws IOException {
         InputStream inputStream;
         inputStream = file.getInputStream();
         return getExcelData(inputStream);
@@ -34,16 +39,16 @@ public class ExcelUtils {
      * @param inputStream
      * @return {@link List< String[]>}
      * @throws
-     * @author zqq
-     * @date 2020/4/16 10:48
+     * @author ZQQ
+     *
      */
-    public static List<String[]> getExcelData(InputStream inputStream) throws IOException, InvalidFormatException {
+    public static List<String[]> getExcelData(InputStream inputStream) throws IOException {
 
         Workbook workbook = WorkbookFactory.create(inputStream);
         Sheet sheet = workbook.getSheetAt(0);
         //getPhysicalNumberOfRows是实际不为空的行数，如果中间有空行，造成最后行的数据无法获取
-        //int rows = sheet.getPhysicalNumberOfRows();
-        int rows = sheet.getLastRowNum();
+        // getLastRowNum从0开始的 所以实际上需要加1
+        int rows = sheet.getLastRowNum() + 1;
         List<String[]> result = new ArrayList<>();
         for (int i = 0; i < rows; i++) {
             Row row = sheet.getRow(i);
@@ -55,16 +60,86 @@ public class ExcelUtils {
             String[] cellBody = new String[cellNum];
             for (int j = 0; j < cellNum; j++) {
                 Cell cell = row.getCell(j);
-                if (null != cell) {
-                    cell.setCellType(CellType.STRING);
-                    cellBody[j] = cell.getStringCellValue();
-                } else {
-                    cellBody[j] = null;
-                    continue;
-                }
+                String cellValue = getCellValue(cell);
+                cellBody[j] = cellValue;
             }
             result.add(cellBody);
         }
         return result;
     }
+
+    /**
+     * 描述:获取cell的值
+     *
+     * @param cell
+     * @return {@link String}
+     * @throws
+     * @author ZQQ
+     *
+     */
+    private static String getCellValue(Cell cell) {
+        String cellValue = null;
+        if (null != cell) {
+            switch (cell.getCellType()) {
+                case NUMERIC: {
+                    if (DateUtil.isCellDateFormatted(cell)) {
+                        Date date = cell.getDateCellValue();
+                        cellValue = DateUtils.formatDateTime(date);
+                    } else {
+                        Object cellValueObject = judgeLongOrDouble(cell.getNumericCellValue());
+                        cellValue = String.valueOf(cellValueObject);
+                    }
+                    break;
+                }
+                case STRING: {
+                    cellValue = cell.getStringCellValue();
+                    break;
+                }
+                case BOOLEAN: {
+                    cellValue = String.valueOf(cell.getBooleanCellValue());
+                    break;
+                }
+                // 公式算出来的值
+                case FORMULA: {
+                    try {
+                        Object cellValueObject = judgeLongOrDouble(cell.getNumericCellValue());
+                        cellValue = String.valueOf(cellValueObject);
+                    } catch (IllegalStateException e) {
+                        cellValue = String.valueOf(cell.getStringCellValue());
+                    }
+                    break;
+                }
+                default: {
+                    cellValue = cell.toString();
+                    break;
+                }
+
+            }
+        }
+        return cellValue;
+    }
+
+
+    /**
+     * 描述:判断excel中数字的类型，excel中的1会被读成1.0，
+     *
+     * @param value
+     * @return {@link Object}
+     * @throws
+     * @author ZQQ
+     *
+     */
+    private static Object judgeLongOrDouble(Double value) {
+        Object cellValueObject;
+        Double doubleVal = value;
+        long longVal = Math.round(doubleVal);
+        if (new BigDecimal(longVal).compareTo(new BigDecimal(doubleVal)) == 0) {
+            cellValueObject = longVal;
+        } else {
+            cellValueObject = doubleVal;
+        }
+        return cellValueObject;
+
+    }
+
 }
